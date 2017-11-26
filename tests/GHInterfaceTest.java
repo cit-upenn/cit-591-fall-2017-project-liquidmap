40900be632214a6144b5601bf6edda4c86c9f068
Brian Edwards <brianedw@seas.upenn.edu>
2017-11-26 18:25:49 -0500

    Added tests to GHInterfaceTest
    Added some abstract tests on things we hope that a Trip has once it has been generated such as point count, increasing time, starting time near zero.

--- tests/GHInterfaceTest.java
+++ tests/GHInterfaceTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class GHInterfaceTest {
@@ -10,7 +13,27 @@ public class GHInterfaceTest {
 		Point p1 = new Point(39.9539856, -75.1868489);
 		Point p2 = new Point(39.9839484, -75.1299003);
 		Trip trip = gh.getTrip(p1, p2);
-		System.out.println(trip);
+
+		// Check that there is a large number of points
+		ArrayList<Point> points = trip.getPoints();
+		int actualSize = points.size();
+		Assert.assertTrue("The trip should have greater than 100pts",
+				actualSize > 100);
+
+		// Check that the first time is 0.0
+		ArrayList<Boolean> increasingTime = new ArrayList<>();
+		double firstTime = points.get(0).getTime();
+		Assert.assertTrue("Should start with time near zero",
+				Math.abs(firstTime - 0.0) < 10E-6);
+
+		// Check that the time is always increasing
+		boolean allGrowing = true;
+		for (int i = 1; i < points.size(); i++) {
+			boolean timeIncQ = points.get(i).getTime() > points.get(i - 1)
+					.getTime();
+			allGrowing &= timeIncQ;
+		}
+		Assert.assertTrue("time should be increasing", allGrowing);
 	}
 
 	@Test
