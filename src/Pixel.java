5c77d74d04b3c641f8baf54217ee16f48acf2066
Brian Edwards <brianedw@seas.upenn.edu>
2017-11-28 21:27:59 -0500

    Added two argument constructor to Pixel
    Many things on the output SVG will utilize pixel as a position, but not as a object with a "color".  For instance, the PointPixel converter doesn't care about the time of the point or the color of the pixel.

--- src/Pixel.java
+++ src/Pixel.java
@@ -1,33 +1,34 @@
 /**
- * This class will represent the pixels in 
- * the image file read in by ImageReader.
- * 
- * A Pixel object will have an (x,y) value corresponding
- * to its 'coordinates' in the image, where the top left 
- * of the image corresponds to an (x,y) of (0,0).  
- * 
- * It will also have a red value corresponding to 
- * the image's color at that location in the red channel.
- * Can vary from 0 - 255.
- * 
- * Using red was chosen arbitrarily.  All maps uploaded
- * to this project will be in greyscale, so
- * we are merely using the red channel to track the 
+ * This class will represent the pixels in the image file read in by
+ * ImageReader.
+ *
+ * A Pixel object will have an (x,y) value corresponding to its 'coordinates' in
+ * the image, where the top left of the image corresponds to an (x,y) of (0,0).
+ *
+ * It will also have a red value corresponding to the image's color at that
+ * location in the red channel. Can vary from 0 - 255.
+ *
+ * Using red was chosen arbitrarily. All maps uploaded to this project will be
+ * in greyscale, so we are merely using the red channel to track the
  * white-to-black gradient.
- * 
+ *
  * @author sgb
  *
  */
 public class Pixel {
-	
+
 	private int redValue;
 	private int pixelX;
 	private int pixelY;
-	
+
 	public Pixel(int pixelX, int pixelY, int redValue) {
 		this.pixelX = pixelX;
 		this.pixelY = pixelY;
 		this.redValue = redValue;
+	}
+
+	public Pixel(int pixelX, int pixelY) {
+		this(pixelX, pixelY, 0);
 	}
 
 	/**
@@ -36,6 +37,7 @@ public class Pixel {
 	public int getRedValue() {
 		return redValue;
 	}
+
 	/**
 	 * @return the pixelX
 	 */
@@ -49,23 +51,26 @@ public class Pixel {
 	public int getPixelY() {
 		return pixelY;
 	}
-	
+
 	/**
-	 * @param redValue the redValue to set
+	 * @param redValue
+	 *            the redValue to set
 	 */
 	public void setRedValue(int redValue) {
 		this.redValue = redValue;
 	}
 
 	/**
-	 * @param pixelX the pixelX to set
+	 * @param pixelX
+	 *            the pixelX to set
 	 */
 	public void setPixelX(int pixelX) {
 		this.pixelX = pixelX;
 	}
 
 	/**
-	 * @param pixelY the pixelY to set
+	 * @param pixelY
+	 *            the pixelY to set
 	 */
 	public void setPixelY(int pixelY) {
 		this.pixelY = pixelY;
