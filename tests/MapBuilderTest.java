dd3c0551eff29fb7dfaf6b3656ecc060e0cde493
Matt Surka <getinthedamnbox@gmail.com>
2017-12-17 21:21:34 -0500

    Removed MainTester; generated new coverage report

--- tests/MapBuilderTest.java
+++ tests/MapBuilderTest.java
@@ -3,7 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class MapBuilderTester {
+public class MapBuilderTest {
 	private MapBuilder mb;
 	
 	@Before
